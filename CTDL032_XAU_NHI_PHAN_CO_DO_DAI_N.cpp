23fd10bc24f7535a5ea48266087c5eff39da31ca
Hoang Tran Phong <48386296+phongthong2003@users.noreply.github.com>
2024-04-21 00:05:19 +0700

    Add files via upload

---
+++ CTDL032_XAU_NHI_PHAN_CO_DO_DAI_N.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+const int mod = 1000000000;
+int f[1111111], n, k;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        memset(f, 0, sizeof(f));
+        cin >> n >> k;
+        f[0] = 2;
+        f[1] = 2;
+        for (int i = 2; i <= n; i++) {
+            if (i <= k) 
+                f[i] = (f[i - 1] * 2) % mod;
+            else 
+                f[i] = (f[i - 1] * 2 - f[i - k - 1]) % mod;
+            if (f[i] < 0) 
+                f[i] += mod;
+        }
+        cout << f[n] << endl;
+    }
+    return 0;
+}
+
