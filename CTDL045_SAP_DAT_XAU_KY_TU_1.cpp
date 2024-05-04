1884335b2e51bb3d6cb5bd72f7e873dcea313947
Hoang Tran Phong <48386296+phongthong2003@users.noreply.github.com>
2024-05-04 20:42:15 +0700

    CTDL045_SAP_DAT_XAU_KY_TU_1.cpp

--- CTDL045_SAP_DAT_XAU_KY_TU_1.cpp
+++ CTDL045_SAP_DAT_XAU_KY_TU_1.cpp
@@ -9,13 +9,15 @@ int main(){
         cin >> s;
         int a[27]={},MAX = 0;
         for(int i = 0; i < s.size(); i++){
-            a[s[i]-'a']++;
+            a[s[i]-'a']++; //nếu s[i] là ký tự 'a', thì a[s[i]-'a']++ tăng số lần xuất hiện của 'a' lên một.
         }
         for(int i = 0; i < 27; i++){
-            MAX = max(MAX,a[i]);
+            MAX = max(MAX,a[i]); //Sau khi đã đếm số lần xuất hiện của mỗi ký tự, vòng lặp tiếp theo duyệt qua mảng a[] để tìm giá trị lớn nhất, lưu vào biến MAX.
         }
         if(MAX * 2 > s.size()+1){
             cout << -1 << endl;
+            //Nếu số lần xuất hiện nhiều nhất của một ký tự nhân đôi lớn hơn kích thước của xâu cộng một 
+            //(MAX * 2 > s.size() + 1), nghĩa là nếu ta có thể sắp xếp các ký tự sao cho hai ký tự giống nhau không kề nhau, chương trình in ra -1.
         }else{
             cout << 1 << endl;
         }
